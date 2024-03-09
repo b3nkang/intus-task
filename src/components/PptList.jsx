b9de6ab5e6197a2203a9f0cb0cbc3512e056cc52
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-03-08 20:35:16 -0500

    feat: add reverse and alpha filtering

--- src/components/PptList.jsx
+++ src/components/PptList.jsx
@@ -5,10 +5,48 @@ import "./styles.scss";
 import { useNavigate } from "react-router-dom";
 import filterDownIcon from "../assets/orderFilter_Down.png";
 import filterUpIcon from "../assets/orderFilter_Up.png";
+import filterAlphaIcon from "../assets/orderFilter_Alpha.png";
 
 function PptList() {
   const { participants } = useParticipants();
   const navigate = useNavigate();
+  const [sortedParticipants, setSortedParticipants] = useState([
+    ...participants,
+  ]);
+  const [sortMode, setSortMode] = useState("default");
+
+  useEffect(() => {
+    let sortedArray = [...participants];
+    if (sortMode === "alphabetical") {
+      sortedArray.sort(
+        (a, b) =>
+          a.firstName.localeCompare(b.firstName) ||
+          a.lastName.localeCompare(b.lastName)
+      );
+    } else if (sortMode === "reverse") {
+      sortedArray.reverse();
+    }
+    setSortedParticipants(sortedArray);
+  }, [participants, sortMode]);
+
+  const toggleSort = () => {
+    setSortMode((prevMode) => {
+      if (prevMode === "default") return "reverse";
+      if (prevMode === "reverse") return "alphabetical";
+      return "default";
+    });
+  };
+
+  const sortIcon = () => {
+    switch (sortMode) {
+      case "reverse":
+        return filterUpIcon;
+      case "alphabetical":
+        return filterAlphaIcon;
+      default:
+        return filterDownIcon;
+    }
+  };
 
   return (
     <div>
@@ -17,11 +55,19 @@ function PptList() {
         <thead>
           <tr>
             <th>Participant's Name</th>
-            <th># of ICD Codes</th>
+            <th>
+              # of ICD Codes
+              <img
+                src={sortIcon()}
+                alt="Sort"
+                onClick={toggleSort}
+                className="sort-icon"
+              />
+            </th>
           </tr>
         </thead>
         <tbody>
-          {participants.map((participant, index) => (
+          {sortedParticipants.map((participant, index) => (
             <tr
               key={index}
               className="clickable-row"
